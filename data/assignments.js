f0341d7315c71c663e366ea8f9e5471d26a34c3d
planit <planit@tanjeffreyz.github.io>
2025-02-10 02:57:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -136,6 +136,13 @@ assignments = {
       "dueDate": "2025-02-13T23:59:00",
       "link": "https://www.gradescope.com/courses/961182",
       "submitted": false
+    },
+    {
+      "title": "PLWS 09.5 - Exam 1 Study Guide",
+      "course": "Chem 3A (Spring 2025)",
+      "dueDate": "2025-02-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/961182",
+      "submitted": false
     }
   ],
   "MATH 53 - DISC 201": [],
