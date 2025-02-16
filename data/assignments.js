5b60dd7bed7218bd8df40c4a273beab7eadd6642
planit <planit@tanjeffreyz.github.io>
2025-02-16 03:01:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -281,6 +281,13 @@ assignments = {
       "dueDate": "2025-02-13T23:59:00",
       "link": "https://www.gradescope.com/courses/965885/assignments/5753481/submissions/308463280",
       "submitted": true
+    },
+    {
+      "title": "HW 4",
+      "course": "Physics 5A",
+      "dueDate": "2025-02-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/965885",
+      "submitted": false
     }
   ]
 };
