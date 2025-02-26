d40d5335143498f977a227962d3382f267b35021
planit <planit@tanjeffreyz.github.io>
2025-02-26 03:00:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -27,6 +27,13 @@ assignments = {
       "dueDate": "2025-02-25T23:59:00",
       "link": "https://www.gradescope.com/courses/959564",
       "submitted": false
+    },
+    {
+      "title": "Homework 5: Review",
+      "course": "ASTRON 98",
+      "dueDate": "2025-03-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/959564",
+      "submitted": false
     }
   ],
   "ASTRON C12/EPS C12/L&SC70T": [
