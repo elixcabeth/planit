c963bb190c109b77de9cbb0eff36746968f3ba73
planit <planit@tanjeffreyz.github.io>
2025-03-19 05:40:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -53,8 +53,8 @@ assignments = {
       "title": "Homework 7: Modules",
       "course": "ASTRON 98",
       "dueDate": "2025-03-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/959564",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/959564/assignments/5629356/submissions/318318900",
+      "submitted": true
     },
     {
       "title": "Homework 5: Review",
