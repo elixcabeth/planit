d0c3497a8c5c86a5feee0954984dbae755ae9a53
planit <planit@tanjeffreyz.github.io>
2025-03-21 06:48:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -351,8 +351,8 @@ assignments = {
       "title": "PLWS 18",
       "course": "Chem 3A (Spring 2025)",
       "dueDate": "2025-03-20T23:59:00",
-      "link": "https://www.gradescope.com/courses/961182",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/961182/assignments/5936848/submissions/318910897",
+      "submitted": true
     },
     {
       "title": "PLWS 20.5 - Exam 2 Study Guide",
